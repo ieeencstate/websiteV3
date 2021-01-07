ffbe02230c987ea13744a6a2c298c0e8c968e87a
isagnik <isagnikbhattacharjee@gmail.com>
2021-01-07 05:26:58 +0400

    about text update

--- src/components/about.js
+++ src/components/about.js
@@ -3,10 +3,8 @@ import React from "react"
 const About = () => {
     return (
        <section className = "aboutsection">
-            <p>Hello whats up chebfekjsjjkkenkjfbekjfbekjbfkjvfkjebf kjewb fkjewb fjb fkjebkjebfkjewbfkjv kjdrbvgbrkj b fejnfe jf e,f ejk vmre,nkf vdr,m krje svnmefnrengroeklfnrie verkjgkre vr,jv kjre gvm,drv rkjgn klre vkrjef kreg kj</p>
-            <p>Hello whats up chebfekjsjjkkenkjfbekjfbekjbfkjvfkjebf kjewb fkjewb fjb fkjebkjebfkjewbfkjv kjdrbvgbrkj b fejnfe jf e,f ejk vmre,nkf vdr,m krje svnmefnrengroeklfnrie verkjgkre vr,jv kjre gvm,drv rkjgn klre vkrjef kreg kj</p>
-            <p>Hello whats up chebfekjsjjkkenkjfbekjfbekjbfkjvfkjebf kjewb fkjewb fjb fkjebkjebfkjewbfkjv kjdrbvgbrkj b fejnfe jf e,f ejk vmre,nkf vdr,m krje svnmefnrengroeklfnrie verkjgkre vr,jv kjre gvm,drv rkjgn klre vkrjef kreg kj</p>
-
+            <p>IEEE at NC State is a student chapter of the Institute of Electrical and Electronics Engineers (IEEE). IEEE is the world's largest professional and academic organization for ECE and CS industries, with the mission of advancing technology for the benefit of humanity.</p>
+            <p>Our primary goal as the NC State IEEE Student Chapter is to engage students and foster meaningful connections with companies. We aim to provide students with a method of access to the latest industry knowledge, whether that be through workshops, technical talks or other events. Lastly, we believe that connecting the student body to industry partners will foster innovation and benefit society as a whole.</p>
        </section>
     )
 }
